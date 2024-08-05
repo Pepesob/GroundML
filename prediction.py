5498d941bbbfe36443942d44a2fced52427d333b
Pepesob <pepesob2@gmail.com>
2024-08-06 00:33:57 +0200

    PC version of soil prediction REST API

--- prediction.py
+++ prediction.py
@@ -1,8 +1,8 @@
-import tflite_runtime.interpreter as tflite
+import tensorflow as tf
+import tensorflow.lite as tflite
 from PIL import Image
 import numpy as np
 from configuration import Configuration
-from camera import take_picture
 
 
 config = Configuration()
@@ -13,19 +13,12 @@ def predictions_with_labels(predictions):
     return {label: float(pred) for label, pred in zip(config["labels"], predictions)}
 
 
-def softmax(x):
-    e_x = np.exp(x - np.max(x))
-    return e_x / e_x.sum(axis=0)
-
-
 def get_image_array(path):
     image = Image.open(path).resize((config["img_width"], config["img_height"]))
-    return np.array(image,dtype=np.float32)
+    return np.array(image, dtype=np.float32)
 
 
 def predict_soil(img_path):
-    take_picture()
-
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
 
@@ -39,7 +32,7 @@ def predict_soil(img_path):
 
     predictions = interpreter.get_tensor(output_details[0]["index"])
 
-    return predictions_with_labels(softmax(predictions[0]))
+    return predictions_with_labels(tf.nn.softmax(predictions[0]))
 
 
 if __name__ == "__main__":
--- rest_server.py
+++ rest_server.py
@@ -13,8 +13,16 @@ async def hello():
 
 @app.get("/predictions")
 async def get_predictions():
-    return predict_soil(config["img_path"])
+    return predict_soil(config["test_img_path"])
 
+@app.post("/predictions/path")
+async def local_image_prediction(path: str):
+    return predict_soil(path)
+
+# @app.post("/predictions/image")
+# async def local_image_prediction(path: str):
+#     print(path)
+#     return predict_soil(path)
 
 if __name__ == '__main__':
     uvicorn.run(app, port=config["port"], host=config["ip_addr"])
