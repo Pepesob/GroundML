fda3f4c9ff84cebd67ed64468ff540200c747f81
Pepesob <pepesob2@gmail.com>
2024-08-09 20:28:49 +0200

    Added multicast address to response on discovery server

--- rest_server.py
+++ rest_server.py
@@ -7,6 +7,7 @@ import socket
 import time
 import asyncio
 import threading
+import struct
 
 
 config = Configuration()
@@ -14,23 +15,28 @@ config = Configuration()
 
 def udp_listen():
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     
-    receive_address = (("0.0.0.0", config["listen_port"]))
+    multicast_group = '239.0.0.1'
+    receive_address = (("", config["listen_port"]))
 
     server_socket.bind(receive_address)
+
+    group = socket.inet_aton(multicast_group)
+    mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+    server_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
     print("Running udp listener on:", receive_address)
 
     while True:
         _, addr = server_socket.recvfrom(1024)
         print("Got request from:", addr)
 
-        for i in range(3):
-            message = "Broadcast message"
-            server_socket.sendto(message.encode("utf-8"), addr)
-            print(f"Sending message number {i}")
-            time.sleep(1)
+        message = "Broadcast message"
+        server_socket.sendto(message.encode("utf-8"), addr)
+        print(f"Sending message to: {addr}")
+        time.sleep(1)
 
 
 loop = asyncio.get_event_loop()
